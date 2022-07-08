89ff1724d9e04268d60d71e21765d67105566ef7
Marek Kaput <marek.kaput@swmansion.com>
2022-07-08 12:13:13 +0200

    reduce random seed entropy to get more human friendly ones

--- protostar/commands/test/testing_seed.py
+++ protostar/commands/test/testing_seed.py
@@ -10,7 +10,7 @@ _testing_seed: ContextVar[Seed] = ContextVar("testing_seed")
 @contextmanager
 def testing_seed(seed: Optional[Seed] = None) -> Iterator[Seed]:
     if seed is None:
-        seed = int.from_bytes(os.urandom(32), byteorder="little", signed=False)
+        seed = int.from_bytes(os.urandom(4), byteorder="little", signed=False)
 
     # HACK: Keep typechecker happy
     assert seed is not None
